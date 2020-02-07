5df979b93fc0227ff2d752aa1e360eb9739ba383
jedoreh <jotham.edoreh@uniben.edu>
2020-02-07 12:33:07 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,66 @@
+const app = require("express")()
+const http = require("http").Server(app)
+const io = require("socket.io")(http)
+const mongoose = require("mongoose")
+
+let users = []
+let messages = []
+//let index = 0
+
+mongoose.connect("mongodb://localhost:27017/chatapp")
+
+const ChatSchema = mongoose.Schema({
+    username: String,
+    msg: String
+})
+
+const ChatModel = mongoose.model("chat", ChatSchema)
+
+ChatModel.find((err, result) => {
+    if (err) throw err
+
+    messages = result
+})
+
+io.on("connection", socket => {
+    socket.emit('loggedIn', {
+        users: users.map(s => s.username),
+        messages: messages
+    })
+
+    socket.on('newuser', username => {
+        console.log(`${username} has arrived at the party.`)
+        socket.username = username
+        users.push(socket)
+
+        io.emit('userOnline', socket.username)
+    })
+
+    socket.on('msg', msg => {
+        let message =  new ChatModel({
+            username: socket.username,
+            msg: msg
+        })
+
+        message.save((err, result) => {
+            if (err) throw err
+            //console.log(result)
+            messages.push(result)
+            io.emit('msg', result)
+        })
+
+    })
+
+    
+    //Disconnect
+    socket.on("disconnect", () => {
+        console.log(`${socket.username} has left the party.`)
+        io.emit('userLeft', socket.username)
+        users.splice(users.indexOf(socket), 1)
+    })
+})
+
+http.listen(process.env.PORT || 3000, () => {
+    console.log("listening on port %s", process.env.PORT || 3000);
+    
+})
